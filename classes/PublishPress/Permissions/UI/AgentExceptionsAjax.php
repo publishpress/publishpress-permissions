e42802b96d042ef7f2c3a912e01ac7e9a2038b3d
Richa Ferry Setyawan <49858604+richaferry@users.noreply.github.com>
2026-05-23 03:30:57 +0700

    Update tooltip "Set as Parent" for terms (#2381)
    Fixes #2359

--- classes/PublishPress/Permissions/UI/AgentExceptionsAjax.php
+++ classes/PublishPress/Permissions/UI/AgentExceptionsAjax.php
@@ -97,7 +97,7 @@ class AgentExceptionsAjax
 
                 if ($for_type === '_term_') {
                     $tooltips = [
-                        'associate' => esc_html__('Control parent selection for selected terms.', 'press-permit-core'),
+                        'associate' => esc_html__('Control which terms can be set as a Parent of other terms.', 'press-permit-core'),
                         'manage'    => esc_html__('Allow users to manage selected terms.', 'press-permit-core'),
                     ];
                 } elseif ($for_source_name === 'pp_group') {
