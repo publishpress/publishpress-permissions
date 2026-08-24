99f93e2a033549e1b603289a7511a62449611759
Richa Ferry Setyawan <aakricha2@gmail.com>
2026-08-24 20:54:30 +0700

    composer update

--- lib/vendor/composer/installed.php
+++ lib/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '2ebf167911b61fb4ee6ef751c607932028a669e7',
+        'reference' => '6d7e0ec13460da307cf9eea676ca8ce117decd61',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '2ebf167911b61fb4ee6ef751c607932028a669e7',
+            'reference' => '6d7e0ec13460da307cf9eea676ca8ce117decd61',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
