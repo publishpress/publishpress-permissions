ad97e3a0b8a488cc371a9e3bb39a0a73390912ba
Steve Burge <steve@rambleventures.com>
2026-09-01 13:41:20 -0400

    fix(a11y): improve permissions settings

--- common/js/settings.dev.js
+++ common/js/settings.dev.js
@@ -1,18 +1,60 @@
 jQuery(document).ready(function ($) {
     // Tabs
     var $tabsWrapper = $('#pp_settings_form ul.nav-tab-wrapper');
-    $tabsWrapper.find('li').click(function (e) {
-        e.preventDefault();
-        $tabsWrapper.children('li').filter('.nav-tab-active').removeClass('nav-tab-active');
-        $(this).addClass('nav-tab-active');
+    var $tabs = $tabsWrapper.find('a[role="tab"]');
 
-        $('.pp-options-wrapper > div').hide();
-        var panel = $(this).find('a').first().attr('href');
-        $(panel).show();
-        
+    function activateTab($tab, moveFocus) {
+        if (!$tab.length) {
+            return;
+        }
+
+        var panel = $tab.attr('href');
+        $tabs.attr('aria-selected', 'false').attr('tabindex', '-1');
+        $tab.attr('aria-selected', 'true').attr('tabindex', '0');
+        $tabsWrapper.children('li').removeClass('nav-tab-active');
+        $tab.closest('li').addClass('nav-tab-active');
+
+        $('.pp-options-wrapper > div').hide().attr('aria-hidden', 'true');
+        $(panel).show().attr('aria-hidden', 'false');
+
         // Update the hidden tab field to maintain current tab after form submission
         var tabName = panel.replace('#pp-', '');
         $('input[name="pp_tab"]').val(tabName);
+
+        if (moveFocus) {
+            $tab.focus();
+        }
+    }
+
+    $tabs.on('click', function (e) {
+        e.preventDefault();
+        activateTab($(this), false);
+    });
+
+    $tabs.on('keydown', function (e) {
+        var currentIndex = $tabs.index(this);
+        var targetIndex = currentIndex;
+
+        if (e.key === 'Enter' || e.key === ' ' || e.keyCode === 13 || e.keyCode === 32) {
+            e.preventDefault();
+            activateTab($(this), false);
+            return;
+        }
+
+        if (e.key === 'ArrowRight' || e.key === 'ArrowDown' || e.keyCode === 39 || e.keyCode === 40) {
+            targetIndex = (currentIndex + 1) % $tabs.length;
+        } else if (e.key === 'ArrowLeft' || e.key === 'ArrowUp' || e.keyCode === 37 || e.keyCode === 38) {
+            targetIndex = (currentIndex - 1 + $tabs.length) % $tabs.length;
+        } else if (e.key === 'Home' || e.keyCode === 36) {
+            targetIndex = 0;
+        } else if (e.key === 'End' || e.keyCode === 35) {
+            targetIndex = $tabs.length - 1;
+        } else {
+            return;
+        }
+
+        e.preventDefault();
+        activateTab($tabs.eq(targetIndex), true);
     });
 
     // todo: pass img url variable, title
@@ -29,7 +71,7 @@ jQuery(document).ready(function ($) {
 
             // Append the image if the conditions are met
             if (hasSubtext && hasVisibleSubtext) {
-                var img_html = '<img class="pp-show-hints" title="See more configuration tips..." src="' + ppCoreSettings.hintImg + '" />';
+                var img_html = '<button type="button" class="pp-show-hints" aria-label="See more configuration tips"><img alt="" src="' + ppCoreSettings.hintImg + '" /></button>';
                 
                 if ($row.find('div.pp-extra-heading').length) {
                     $row.find('div.pp-extra-heading').before(img_html);
@@ -41,7 +83,7 @@ jQuery(document).ready(function ($) {
             }
         });
 
-        $('.pp-options-table tr img.pp-show-hints').click(function() {
+        $('.pp-options-table tr button.pp-show-hints').click(function() {
             $(this).closest('tr').find('td .pp-subtext, td .pp-hint, table.pp-hint, div.pp-hint').show();
             $(this).hide();
         });
